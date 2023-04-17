f546078b4fa5d0389c8c3fa2ea4ce8c78cf274dc
Sylphri <sylphri@gmail.com>
2023-04-17 18:02:25 +0300

    Add main menu and lose menu.

--- Assets/Scripts/CustomField.cs
+++ Assets/Scripts/CustomField.cs
@@ -22,7 +22,7 @@ public class CustomField : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                Vector3 pos = new Vector3(i - width / 2f + 0.5f, j - height / 2f + 0.5f, 0) + transform.position;
+                Vector3 pos = new Vector3(i - width / 2f + 0.5f, j - height / 2f + 0.5f, -1f) + transform.position;
                 Instantiate(_slotPrefab, pos, Quaternion.identity, _slotsParent);
             }
         }
